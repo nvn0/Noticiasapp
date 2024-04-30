9657b04a3d5729a308335635ca076334db4e17e6
nuno <75589883+nvn0@users.noreply.github.com>
2024-04-30 01:48:44 +0100

    initial commit

---
+++ Noticiasapp/Models/IndexModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Noticiasapp.Models
+{
+    public class IndexModel
+    {
+        public void OnGet()
+        {
+            // Lógica de obtenção dos dados, se necessário
+        }
+    }
+}
