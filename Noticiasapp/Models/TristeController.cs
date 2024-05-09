6d11681e5be812f2a2be795a6ccee0e11f6ad7fb
nuno <75589883+nvn0@users.noreply.github.com>
2024-05-09 03:20:02 +0100

    nova pagina e separaçãode noticias
     formatação precisa de ser ajustada

---
+++ Noticiasapp/Models/TristeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Noticiasapp.Models
+{
+    public class TristeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Noticiasapp/Pages/Triste.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Noticiasapp.Pages
+{
+    public class TristeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
